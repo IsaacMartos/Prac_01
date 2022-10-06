0ea61a258fc5e5395077177db0ba5cda4fbb0180
IsaacMartos <imartosf@edu.tecnocampus.cat>
2022-10-06 23:50:13 +0200

    aaaa

--- Code/Assets/Code/FFPlayerController.cs
+++ Code/Assets/Code/FFPlayerController.cs
@@ -25,10 +25,10 @@ public class FFPlayerController : MonoBehaviour
     public KeyCode m_DownKeyCode;
     public KeyCode m_JumpKeyCode = KeyCode.Space;
     public KeyCode m_RunKeyCode = KeyCode.LeftShift;
-
-    public Camera m_Camera;
-    public float m_NormalMovementFOV = 60.0f;
-    public float m_RunMovementFOV = 75.0f;
+    public KeyCode m_DebugLockAngleKeyCode = KeyCode.I;
+    public KeyCode m_DebugLockKeyCode = KeyCode.O;
+    bool m_AngleLocked = false;
+    bool m_AimLocked = true;
 
 
     float m_VerticalSpeed = 0.0f;
@@ -36,15 +36,60 @@ public class FFPlayerController : MonoBehaviour
 
     public float m_JumpSpeed = 10.0f;
 
+    public GameObject m_DecalPrefab;
+
+    [Header("Camera")]
+    public Camera m_Camera;
+    public float m_NormalMovementFOV = 60.0f;
+    public float m_RunMovementFOV = 75.0f;
+
+    [Header("Shoot")]
+    public float m_MaxShootDistance;
+    public LayerMask m_ShootinLayerMask;
+
+    [Header("Animations")]
+    public Animation m_Animations;
+    public AnimationClip m_ShootingAnimationClip;
+    public AnimationClip m_IdleAnimationClip;
+    public AnimationClip m_ReloadAnimationClip;
+    private bool m_Shooting = false;
+
+    float m_Life;
+
+    //añadir tiempo animaciones para no tener que hardcodearlo
     void Start()
     {
+        m_Life = GameController.GetGameController().SetPlayerLife();
         m_Yaw = transform.rotation.y;
         m_Pitch = m_PitchController.localRotation.x;
-    }
+        Cursor.lockState = CursorLockMode.Locked;
+        m_AimLocked = Cursor.lockState == CursorLockMode.Locked;
+        SetIdleWeaponAnimation();
 
+    }
+#if UNITY_EDITOR
+    void UpdateInputDebug()
+    {
+        if (Input.GetKeyDown(m_DebugLockAngleKeyCode))
+        {
+            m_AngleLocked = !m_AngleLocked;
+        }
+        if (Input.GetKeyDown(m_DebugLockKeyCode))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+                Cursor.lockState = CursorLockMode.None;
+            else
+                Cursor.lockState = CursorLockMode.Locked;
+            m_AimLocked = Cursor.lockState == CursorLockMode.Locked;
+        }
+    }
+#endif
 
     void Update()
     {
+#if UNITY_EDITOR
+        UpdateInputDebug();
+#endif
         //Movement
         Vector3 l_RightDirection = transform.right;
         Vector3 l_ForwardDirection = transform.forward;
@@ -78,6 +123,14 @@ public class FFPlayerController : MonoBehaviour
         float l_MouseX = Input.GetAxis("Mouse X");
         float l_MouseY = Input.GetAxis("Mouse Y");
 
+#if UNITY_EDITOR
+        if (m_AngleLocked)
+        {
+            l_MouseX = 0.0f;
+            l_MouseY = 0.0f;
+        }
+#endif
+
         m_Yaw = m_Yaw + l_MouseX * m_YawRotationlSpeed * Time.deltaTime * (m_UseYawInverted ? -1.0f : 1.0f);
         m_Pitch = m_Pitch + l_MouseY * m_PitchRotationlSpeed * Time.deltaTime * (m_UsePitchInverted ? -1.0f : 1.0f);
         m_Pitch = Mathf.Clamp(m_Pitch, m_MinPitch, m_MaxPitch);
@@ -98,5 +151,49 @@ public class FFPlayerController : MonoBehaviour
         }
         else
             m_OnGround = false;
+
+        if (Input.GetMouseButtonDown(0) && CanShoot())
+        {
+            Shoot();
+        }
+    }
+
+    private bool CanShoot()
+    {
+        return !m_Shooting;
+    }
+
+    void Shoot()
+    {
+        Ray l_Ray = m_Camera.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+        RaycastHit l_RayCastHit;
+        if (Physics.Raycast(l_Ray, out l_RayCastHit, m_MaxShootDistance, m_ShootinLayerMask.value))
+            CreateShootingParticles(l_RayCastHit.collider, l_RayCastHit.point, l_RayCastHit.normal);
+        SetWeaponShootAnimation();
+        m_Shooting = true;
+    }
+
+    void CreateShootingParticles(Collider _Collider, Vector3 Position, Vector3 Normal)
+    {
+        //Debug.DrawRay(Position, Normal * 5.0f, Color.red, 2.0f);
+        GameObject.Instantiate(m_DecalPrefab, Position, Quaternion.LookRotation(Normal));
+    }
+
+    void SetIdleWeaponAnimation()
+    {
+        m_Animations.CrossFade(m_IdleAnimationClip.name);
+    }
+
+    void SetWeaponShootAnimation()
+    {
+        m_Animations.CrossFade(m_ShootingAnimationClip.name, 0.1f);
+        m_Animations.CrossFadeQueued(m_IdleAnimationClip.name, 0.1f);
+        StartCoroutine(EndShoot());
+    }
+
+    IEnumerator EndShoot()
+    {
+        yield return new WaitForSeconds(m_ShootingAnimationClip.length);
+        m_Shooting = false;
     }
 }
---
+++ Code/Assets/Code/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private void Start()
+    {
+        GameController.DestroySingleton();
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public void OnStartClicked()
+    {
+        SceneManager.LoadScene("Level1Scene");
+    }
+
+}
