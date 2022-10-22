16d9d14d8d62f6627d8c793f37d026f663b7b772
IsaacMartos <imartosf@edu.tecnocampus.cat>
2022-10-22 18:10:56 +0200

    aaaa

--- Code/Assets/Code/DoorController.cs
+++ Code/Assets/Code/DoorController.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class DoorController : MonoBehaviour
 {
     public float m_DoorDistance = 2.0f;
