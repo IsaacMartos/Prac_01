4f99718b8031ce1fc9d331a70c0a8a5c33d984fd
IsaacMartos <imartosf@edu.tecnocampus.cat>
2022-10-23 14:46:12 +0200

    a

--- Code/Assets/PlatformAttach.cs
+++ Code/Assets/PlatformAttach.cs
@@ -3,29 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformAttach : MonoBehaviour
-{
-	//public GameObject Player;
-	GameController Player;
+{	
 
-	private void OnTriggerEnter(Collider other)
+	public void OnTriggerEnter(Collider other)
 	{
-		/*if (other.gameObject == Player)
-			Player.transform.parent = transform;*/
-		/*if (other.transform.tag == "Player")
-			other.transform.SetParent(transform);*/
-		if (other.gameObject == Player.GetPlayer())
-			Player.transform.parent = transform;
-	}
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.transform.SetParent(transform);
+        }
+    }
+	/*if (other.gameObject == Player)
+		Player.transform.parent = transform;*/
+	/*if (other.transform.tag == "Player")
+		other.transform.SetParent(transform);*/
+
 
 	private void OnTriggerExit(Collider other)
 	{
-		/*if (other.gameObject == Player)
+        /*if (other.gameObject == Player)
 			Player.transform.parent = null;*/
-		/*if (other.transform.tag == "Player")
+        /*if (other.transform.tag == "Player")
 			other.transform.SetParent(null);*/
-		if (other.gameObject == Player.GetPlayer())
-			Player.transform.parent = null;
-	}
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.transform.SetParent(null);
+        }
+    }
 
 
 }
