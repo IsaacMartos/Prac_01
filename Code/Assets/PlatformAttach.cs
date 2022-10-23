5f777881a4dcfa681492193f139934e818b82791
Andrea-PY18 <apadillay@edu.tecnocampus.cat>
2022-10-23 12:44:34 +0200

    no entiendo

--- Code/Assets/PlatformAttach.cs
+++ Code/Assets/PlatformAttach.cs
@@ -5,21 +5,26 @@ using UnityEngine;
 public class PlatformAttach : MonoBehaviour
 {
 	//public GameObject Player;
+	GameController Player;
 
 	private void OnTriggerEnter(Collider other)
 	{
 		/*if (other.gameObject == Player)
 			Player.transform.parent = transform;*/
-		if (other.tag == "Player")
-			other.transform.SetParent(transform);
+		/*if (other.transform.tag == "Player")
+			other.transform.SetParent(transform);*/
+		if (other.gameObject == Player.GetPlayer())
+			Player.transform.parent = transform;
 	}
 
 	private void OnTriggerExit(Collider other)
 	{
 		/*if (other.gameObject == Player)
 			Player.transform.parent = null;*/
-		if (other.tag == "Player")
-			other.transform.SetParent(null);
+		/*if (other.transform.tag == "Player")
+			other.transform.SetParent(null);*/
+		if (other.gameObject == Player.GetPlayer())
+			Player.transform.parent = null;
 	}
 
 
