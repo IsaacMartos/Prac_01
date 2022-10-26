9b4e86809c58f33d1660e6c635a963c995f071f8
Andrea-PY18 <apadillay@edu.tecnocampus.cat>
2022-10-26 20:24:30 +0200

    sonidos

--- Code/Assets/Code/AmmoItem.cs
+++ Code/Assets/Code/AmmoItem.cs
@@ -5,10 +5,16 @@ public class AmmoItem : Item
 {
 	public float m_Ammo;
 	public GameObject Particulas;
+	private AudioManager AudioManager;
+	private void Start()
+	{
+		AudioManager = FindObjectOfType<AudioManager>();
+	}
 	public override void Pick(FFPlayerController Player)
 	{
 		if (Player.GetAmmo() < 50.0f)
 		{
+			AudioManager.SeleccionAudio(5, 1.5f);
 			Player.AddAmmo(m_Ammo);
             //GameController.GetGameController().AddRespawnItemsElement(this);
             gameObject.SetActive(false);
